bdf6167c4765688992d94653168698152722fe29
Rodney van den Velden <rodneymichael2002@gmail.com>
2022-11-29 16:35:29 +0100

    senior 1

--- src/nl/novi/jp/methods/senior/SeniorOne.java
+++ src/nl/novi/jp/methods/senior/SeniorOne.java
@@ -18,18 +18,45 @@ import java.util.List;
 
 public class SeniorOne {
     public static void main(String[] args) {
-        List<String> curstomerNames = new ArrayList<>();
+        List<String> customerNames = new ArrayList<>();
 
-        curstomerNames.add("nick piraat");
-        curstomerNames.add("michael jackson");
-        curstomerNames.add("glennis grace");
-        curstomerNames.add("dreetje hazes");
-        curstomerNames.add("robbie williams");
-        curstomerNames.add("michiel de ruyter");
-        curstomerNames.add("sjaak polak");
-        curstomerNames.add("jan van jansen");
-        curstomerNames.add("henk den hartog");
-        curstomerNames.add("mo el-mecky");
-        curstomerNames.add("fredje kadetje");
+        customerNames.add("nick piraat");
+        customerNames.add("michael jackson");
+        customerNames.add("glennis grace");
+        customerNames.add("dreetje hazes");
+        customerNames.add("robbie williams");
+        customerNames.add("michiel de ruyter");
+        customerNames.add("sjaak polak");
+        customerNames.add("jan van jansen");
+        customerNames.add("henk den hartog");
+        customerNames.add("mo el-mecky");
+        customerNames.add("fredje kadetje");
+
+        customerNames = capitalizeNames(customerNames);
+        System.out.println(customerNames);
+    }
+
+    public static List<String> capitalizeNames(List<String> list) {
+        List<String> customerNames = new ArrayList<>();
+
+        for (String name : list) {
+            char[] charArray = name.toCharArray();
+            boolean foundSpace = true;
+
+            for(int i = 0; i < charArray.length; i++) {
+                if (Character.isLetter(charArray[i])) {
+                    if (foundSpace) {
+                        charArray[i] = Character.toUpperCase(charArray[i]);
+                        foundSpace = false;
+                    }
+                } else {
+                    foundSpace = true;
+                }
+            }
+            name = String.valueOf(charArray);
+            customerNames.add(name);
+        }
+
+        return customerNames;
     }
 }
