094bd907b1f7c163f55adee5992c03f69a258796
Rodney van den Velden <rodneymichael2002@gmail.com>
2022-11-29 16:03:54 +0100

    beginner done

--- src/nl/novi/jp/methods/beginner/BeginnerFive.java
+++ src/nl/novi/jp/methods/beginner/BeginnerFive.java
@@ -6,4 +6,11 @@ package nl.novi.jp.methods.beginner;
  * Zorg ervoor dat de methode wordt aangeroepen en dat de tekst uitgeprint wordt.
  */
 public class BeginnerFive {
+  public static void start() {
+    System.out.println("Power on");
+  }
+
+  public static void main(String[] args) {
+    start();
+  }
 }
